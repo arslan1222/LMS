4ff259f3e7d05fb94fa2ad30a814f74f7dfa4f82
Arslan Haider <arslanhaidermughal8@gmail.com>
2025-03-30 11:25:00 +0500

    Course Details page added

--- frontend/src/Context/AppContext.jsx
+++ frontend/src/Context/AppContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { dummyCourses } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
+import humanizeDuration from 'humanize-Duration'
 
 
 export const AppContext = createContext();
@@ -31,6 +32,28 @@ export const AppContextProvider = (props) => {
         return totalRating / course.courseRatings.length;
     }
 
+    // Caculate course chapter time
+
+    const calculateChapterTime = (chapter) => {
+        let time = chapter.chapterContent.reduce((total, lecture) => total + lecture.lectureDuration, 0);
+        
+        return humanizeDuration(time * 60 * 1000, { units: ['h', 'm'] });
+    };
+
+    // Calculate the total course duration
+    const calculateCourseDuration = (course) => {
+        let time = course.courseContent.reduce((total, chapter) => {
+            return total + chapter.chapterContent.reduce((chapTotal, lecture) => chapTotal + lecture.lectureDuration, 0);
+        }, 0);
+
+        return humanizeDuration(time * 60 * 1000, { units: ['h', 'm'] });
+    };
+
+    // Calculate total number of lectures in the course
+    const calculateLectures = (course) => {
+        return course.courseContent.reduce((total, chapter) => total + (Array.isArray(chapter.chapterContent) ? chapter.chapterContent.length : 0), 0);
+    };
+
     useEffect(()=>{
         fetchAllCOurses();
     }, []);
@@ -42,6 +65,7 @@ export const AppContextProvider = (props) => {
         navigate,
         calculateRating,
         isEducator, setIsEducator,
+        calculateChapterTime, calculateLectures, calculateCourseDuration
     }
 
     return (
