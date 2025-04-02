eac3b234b07cb5fee058f8e571bf61b50affbe79
Arslan Haider <arslanhaidermughal8@gmail.com>
2025-04-02 17:05:58 +0500

    Dashboard page created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Routes, useMatch } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './Pages/Student/Home'
 import CoursesList from './Pages/Student/CoursesList'
 import CourseDetails from './Pages/Student/CourseDetails'
@@ -11,16 +11,16 @@ import DashBoard from './Pages/Educator/DashBoard'
 import AddCourse from './Pages/Educator/AddCourse'
 import MyCourses from './Pages/Educator/MyCourses'
 import StudentsEnrolled from './Pages/Educator/StudentsEnrolled'
-import { assets } from './assets/assets'
-import Navbar from './Components/Educator/Navbar'
+import Navbar from './Components/Student/Navbar'
 
 const App = () => {
-
-  const isEducatorRoute = useMatch('/educator') // When the path match the educator will be true
+  const location = useLocation();
+  const isEducatorRoute = location.pathname.startsWith('/educator');
 
   return (
     <div className='text-default min-h-screen bg-white'>
-      {!isEducatorRoute && <Navbar /> }
+      {/* Show Student Navbar only when NOT on educator routes */}
+      {!isEducatorRoute && <Navbar />}
       
       <Routes>
         <Route path='/' element={<Home />} />
@@ -31,17 +31,15 @@ const App = () => {
         <Route path='/player/:id' element={<Player />} />
         <Route path='/Loading/:path' element={<Loading />} />
 
-        <Route path='/educator' element={<Educator />} >
-          <Route path='educator' element={<DashBoard />} />
+        <Route path='/educator/*' element={<Educator />}>
+          <Route index element={<DashBoard />} />
           <Route path='add-course' element={<AddCourse />} />
           <Route path='my-courses' element={<MyCourses />} />
           <Route path='student-enrolled' element={<StudentsEnrolled />} />
         </Route>
-
       </Routes>
-
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
