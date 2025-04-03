990e41f7e6bc9ab056e94417cfa067858d25425d
Arslan Haider <arslanhaidermughal8@gmail.com>
2025-04-03 13:04:52 +0500

    Webhooks controller created

--- backend/server.js
+++ backend/server.js
@@ -2,19 +2,22 @@ import express from "express";
 import cors from "cors"
 import 'dotenv/config'
 import connectDB from "./config/connectDB.js";
-
+import { clerkWebhooks } from "./controllers/webhooks.js";
 const app = express();
+
+const PORT = process.env.PORT || 3000;
+
 
 app.use(cors());
 
 
 connectDB();
 
-const PORT = process.env.PORT || 3000;
-
 app.get('/', (req, res)=>{
     res.send("I am Root!")
-})
+});
+
+app.post('/clerk', express.json(), clerkWebhooks);  // request will be parsed using json method
 
 
 app.listen(PORT, ()=>{
